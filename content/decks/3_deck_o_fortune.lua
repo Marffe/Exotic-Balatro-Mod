ce3e6bc24b9dff88f48473ff6c8f51e95627e5db
Marffe <efframlol@gmail.com>
2025-11-23 10:20:53 -0500

    Added base for 2 new Decks

---
+++ content/decks/3_deck_o_fortune.lua
@@ -0,0 +1,8 @@
+SMODS.Back {
+    key = "deck_o_fortune", atlas = "joker_atlas", pos = { x = 4, y = 2 },
+
+    config = { discards = 1 },
+    loc_vars = function(self, info_queue, back)
+        return { vars = { self.config.discards } }
+    end,
+}
