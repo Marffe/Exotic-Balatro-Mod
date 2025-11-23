d9d68a403379ba46b346aa61003608970e906cbc
Marffe <efframlol@gmail.com>
2025-11-23 13:48:35 -0500

    Starting the new Update

---
+++ content/decks/5_obelisk.lua
@@ -0,0 +1,8 @@
+SMODS.Back {
+    key = "obelisk_deck", atlas = "joker_atlas", pos = { x = 4, y = 2 },
+
+    config = { discards = 1 },
+    loc_vars = function(self, info_queue, back)
+        return { vars = { self.config.discards } }
+    end,
+}
---
+++ content/jokers/12_Fishing_rod.lua
@@ -0,0 +1,14 @@
+SMODS.Joker {
+    key = "Fishing_rod", atlas = "joker_atlas", pos = { x = 0, y = 0 }, -- Common
+
+    blueprint_compat = true, eternal_compat = true, perishable_compat = true,
+    rarity = 1, cost = 4, discovered = true, unlocked = true,
+
+    config = { 
+        extra = { } 
+    },
+
+    loc_vars = function(self, info_queue, card)
+        return { vars = { } }
+    end,
+}
